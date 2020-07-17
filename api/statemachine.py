17680cc1089d2c9a33f5fbcbd2ace8f3922fb094
njriasan <njriasanovsky@berkeley.edu>
2020-07-16 22:13:08 -0700

    a few more small changes, doesn't use input/output properly

--- api/statemachine.py
+++ api/statemachine.py
@@ -1,6 +1,6 @@
 from typing import Callable, Iterable, Mapping
 
-#forward declare
+#forward declare of State to trick the typing module
 class State:
     def __init__(self):
         pass
@@ -9,32 +9,66 @@ class State:
 class StateMachine:
     
     def __init__(self, input_vars: Mapping[str, int], output_vars: Mapping[str, int],
-            states: Iterable[State]):
+            states: List[State]):
+        # No need to store the list of states but we will just in case
+        # compiling becomes easier/for debugging
+        self._states = states
+
         # Check uniqueness across variable names
         input_vars_set = set(input_vars.key())
         output_vars_set = set(output_vars.key())
         total_set = input_vars_set.union(output_vars_set)
         if len(total_set) != len(input_vars_set) + len(output_vars_set):
-            raise KeyError("Variable name shared between input and output variables")
+            raise RuntimeError("Error: Variable name shared between input and output variables.")
         self._input_vars = input_vars
         self._output_vars = output_vars
         # Check all of the states for uniqueness
+        names_set = set()
+        initial_counter = 0
         for state in states:
-            # TODO: Add a check to ensure there is exactly one starting state
-            # TODO: Check uniqueness by name
-            pass
-
-        # TODO: Add a starting state and current state (possibly redundant)
+            names_set.append(state.getName())
+            is_initial = state.isInitial()
+            if is_initial:
+                initial_counter += 1
+                # Add a current and starting state. The starting state might
+                # be redundnant
+                self._start_state = state
+                self._curr_state = state
+        # Check that there are no duplicate names
+        if len(names_set) != len(states):
+            raise RuntimeError("Error: Not all states provided have a unique name.")
+        # Check that there is exactly 1 start state
+        if initial_counter > 1:
+            raise RuntimeError("Error: Multiple start states provided.")
+        elif initial_counter == 0:
+            raise RuntimeError("Error: No start state provided.")
 
     def tick(self):
-        # TODO implement once state is properly defined
+        # TODO: Figure out how to include the input/output
+        # vars as available in each function        
+        state = getCurrState()
+
         # Calls the action function of the state
+        state.action()
+
         # Checks transitions
-        # Updates the current state
-        pass
+        for transition in state.getTransitions():
+            if transition.checkCondition():
+                # Updates the current state
+                self._curr_state = self.getNextState()
+                break
 
     def getOutputVar(self, var_name: str):
         return self._output_vars[var_name]
+
+    def getStartState(self) -> State:
+        return self._start_state
+
+    def getCurrState(self) -> State:
+        return self._curr_state
+
+    def getStates(self) -> List[State]:
+        return self._states
 
 # Class holding the information for a particular transition
 class Transition:
@@ -52,10 +86,11 @@ class Transition:
 # Class holding the information about a particular state
 class State:
 
-    def __init__(self, name: str, action_func: Callable [[], None]):
+    def __init__(self, name: str, action_func: Callable [[], None], initial=False: bool):
         self._name = name
         self._transitions = []
         self._action_func = action_func
+        self._initial = initial
 
     def getName(self) -> str:
         return self._name
@@ -63,8 +98,11 @@ class State:
     def addTransition(self, transition: Transition):
         self._transitions.append(transition)
 
-    def getTransitions(self) -> Iterable[Transition]:
+    def getTransitions(self) -> List[Transition]:
         return self._transitions
 
     def action(self):
         self._action_func()
+
+    def isInitial(self) -> bool:
+        return self._initial
