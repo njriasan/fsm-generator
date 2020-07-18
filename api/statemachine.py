813e69f6b10fe793f6b3bd82e1c78a6f3835f217
njriasan <njriasanovsky@berkeley.edu>
2020-07-18 03:05:58 -0700

    modifications to the state machine api have no syntax errors

--- api/statemachine.py
+++ api/statemachine.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, Mapping
+from typing import Callable, List, Mapping
 
 #forward declare of State to trick the typing module
 class State:
@@ -8,20 +8,22 @@ class State:
 # Class holding the information for the overall state machine
 class StateMachine:
     
-    def __init__(self, input_vars: Mapping[str, int], output_vars: Mapping[str, int],
-            states: List[State]):
+    def __init__(self, input_vars: Mapping[str, int], output_vars: Mapping[str, int], 
+            defined_vars: Mapping[str, int], states: List[State]):
         # No need to store the list of states but we will just in case
         # compiling becomes easier/for debugging
         self._states = states
 
         # Check uniqueness across variable names
-        input_vars_set = set(input_vars.key())
-        output_vars_set = set(output_vars.key())
-        total_set = input_vars_set.union(output_vars_set)
-        if len(total_set) != len(input_vars_set) + len(output_vars_set):
-            raise RuntimeError("Error: Variable name shared between input and output variables.")
+        input_vars_set = set(input_vars.keys())
+        output_vars_set = set(output_vars.keys())
+        defined_vars_set = set(defined_vars.keys())
+        total_set = input_vars_set.union(output_vars_set).union(defined_vars_set)
+        if len(total_set) != len(input_vars_set) + len(output_vars_set) + len(defined_vars):
+            raise RuntimeError("Error: Variable name shared between input, output, and defined variables.")
         self._input_vars = input_vars
         self._output_vars = output_vars
+        self._defined_vars = defined_vars
         # Check all of the states for uniqueness
         names_set = set()
         initial_counter = 0
@@ -49,11 +51,11 @@ class StateMachine:
         state = getCurrState()
 
         # Calls the action function of the state
-        state.action()
+        state.action(self._input_vars, self._output_vars, self._defined_vars)
 
         # Checks transitions
         for transition in state.getTransitions():
-            if transition.checkCondition():
+            if transition.checkCondition(self._input_vars, self._output_vars, self._defined_vars):
                 # Updates the current state
                 self._curr_state = self.getNextState()
                 break
@@ -73,12 +75,12 @@ class StateMachine:
 # Class holding the information for a particular transition
 class Transition:
     
-    def __init__(self, cond_func: Callable [[], bool], next_state: State):
+    def __init__(self, cond_func: Callable [[Mapping[str, int], Mapping[str, int], Mapping[str, int]], bool], next_state: State):
         self._cond_func = cond_func
         self._next_state = next_state
 
-    def checkCondition(self) -> bool:
-        return self._cond_func()
+    def checkCondition(self, input_vars, output_vars, defined_vars) -> bool:
+        return self._cond_func(input_vars, output_vars, defined_vars)
 
     def getNextState(self) -> State:
         return self._next_state
@@ -86,7 +88,8 @@ class Transition:
 # Class holding the information about a particular state
 class State:
 
-    def __init__(self, name: str, action_func: Callable [[], None], initial=False: bool):
+    def __init__(self, name: str, action_func: Callable [[Mapping[str, int], Mapping[str, int], Mapping[str, int]], None], 
+            initial: bool = False):
         self._name = name
         self._transitions = []
         self._action_func = action_func
@@ -101,8 +104,8 @@ class State:
     def getTransitions(self) -> List[Transition]:
         return self._transitions
 
-    def action(self):
-        self._action_func()
+    def action(self, input_var, output_vars, defined_vars):
+        self._action_func(input_vars, output_vars, defined_vars)
 
     def isInitial(self) -> bool:
         return self._initial
